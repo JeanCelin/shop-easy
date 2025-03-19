adae502757155e80438d9ef1a7ae92093c3e0588
Jean Celin <jean_estudos@hotmail.com>
2025-03-19 08:45:20 -0300

    create project structure

---
+++ shopeasy/src/lib/dbConnect.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error('Defina a variável de ambiente MONGO_URI');
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+export default async function dbConnect() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
